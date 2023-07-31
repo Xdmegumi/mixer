7d56b906e6abc6201ae1f670318943c2d0c1a8ce
Chenyu Gao <ncvxopt@gmail.com>
2023-07-31 16:29:45 +0800

    Update Loading.svelte

--- src/components/Loading.svelte
+++ src/components/Loading.svelte
@@ -2,8 +2,7 @@
   <p class="text-3xl md:text-9xl m-auto flex flex-col max-w-[80%]">
     <span class="text-center">Loading Tracks</span>
     <span class="text-base md:text-lg block m-auto mt-7 text-center">
-      This may take a short while on your first time recording your memory
-      on Nomai Statue.
+      正在将记忆上传到挪麦雕像...
     </span>
     <span class="loader" />
   </p>
